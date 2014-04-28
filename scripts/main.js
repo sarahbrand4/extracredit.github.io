85badab05788455b704def5fadb39cd24ce27872
sarahbrand4 <sarah2@Sarahs-MacBook-Pro.local>
2014-04-28 18:40:26 -0400

    Added show/hide jquery to recipe page

--- scripts/main.js
+++ scripts/main.js
@@ -36,5 +36,39 @@ $(document).ready(function(){
         
         
     });
+    
+    
+      $(".info").hide(); 
+
+
+ 
+$("#recipe1").on("click", function() {
+	$(".one").slideToggle("slow");
+});
+
+ 
+$("#recipe2").on("click", function() {
+	$(".two").slideToggle("slow");
+});
+
+ 
+$("#recipe3").on("click", function() {
+	$(".three").slideToggle("slow");
+});
+
+
+$("#recipe4").on("click", function() {
+	$(".four").slideToggle("slow");
+});
+    
+
+$("#recipe5").on("click", function() {
+	$(".five").slideToggle("slow");
+});   
+   
+ 
+$("#recipe6").on("click", function() {
+	$(".six").slideToggle("slow");
+});    
  
 });
