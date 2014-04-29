fffa1fb588cecf3acdde7096b5ad58b952c71064
sarahbrand4 <sarah2@Sarahs-MacBook-Pro.local>
2014-04-28 22:09:24 -0400

    Finished website

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,5 @@
 $(document).ready(function(){
 
-    
-    	$('nav ul li a').click(function(){
- 	
- 		var el = $(this).attr('href');
- 		var elWrapped = $(el);
- 		
- 		scrollToDiv(elWrapped,40);
- 		
- 		return false;
- 	
- 	});
- 	
- 	function scrollToDiv(element,navheight){
- 	
- 		
- 	
- 		var offset = element.offset();
- 		var offsetTop = offset.top;
- 		var totalScroll = offsetTop-navheight;
- 		
- 		$('body,html').animate({
- 				scrollTop: totalScroll
- 		}, 500);
- 	
- 	}
- 	
- 
-
-    $(function(){
-        var s = skrollr.init({
-            render: function(data) {
-            }
-        });
-        
-        
-    });
-    
-    
       $(".info").hide(); 
 
 
